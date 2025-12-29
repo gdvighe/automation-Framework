99bb2009926623f6ac5021cd98406a70a30251ba
gdvighe <gdvighe@gmail.com>
2025-12-29 21:04:51 +0530

    changes done in  takescreenshot method

--- src/test/java/com/Utility/BrowserUtility.java
+++ src/test/java/com/Utility/BrowserUtility.java
@@ -140,7 +140,7 @@ public class BrowserUtility {
 
 		SimpleDateFormat format = new SimpleDateFormat("HH-mm-ss");
 		String timeStamp = format.format(date);
-		String path =  "./screenshot/" + name + "-" + timeStamp + ".png";
+		String path =  "./screenshots/" + name + "-" + timeStamp + ".png";
 		File screenshotFile = new File(path);
 		try {
 			FileUtils.copyFile(screenshotData, screenshotFile);
