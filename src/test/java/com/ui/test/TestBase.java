dc2642b284b3a4f7f18b7f0fc453e255e60e881c
gdvighe <gdvighe@gmail.com>
2025-12-30 22:19:55 +0530

    Change default headless option to false in setup

--- src/test/java/com/ui/test/TestBase.java
+++ src/test/java/com/ui/test/TestBase.java
@@ -27,7 +27,7 @@ public class TestBase {
 	public void setup(
 		@Optional("chrome")	String browser, 
 		@Optional("true")	boolean isLambdaTest, 
-		@Optional("true")	boolean isHeadless, ITestResult result) {
+		@Optional("false")	boolean isHeadless, ITestResult result) {
 
 		this.isLambdaTest = isLambdaTest;
 		WebDriver lambdaDriver;
